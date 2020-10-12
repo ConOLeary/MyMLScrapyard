484ebe154bab2d434ca247d1a296909fef4bee80
ConOLeary <conolaoghaire@gmail.com>
2020-10-12 20:40:02 +0100

    Various changes. Thinking of experimenting with a different learning rate for b and m

--- week1/main2.py
+++ week1/main2.py
@@ -4,7 +4,7 @@ from numpy import *
 import pygal
 import sys
 
-SHOW_EVERY_NSTEPS = 500
+SHOW_EVERY_NSTEPS = 100
 
 graf1 = pygal.XY(stroke=False)
 graf2 = pygal.XY(stroke=False)
@@ -17,7 +17,7 @@ def compute_error_for_line_given_points(b, m, points):
     totalError = 0
     for i in range(0, len(points)):
         x = i
-        y = points[i]
+        y = points[i][1]
         totalError += (y - (m * x + b)) ** 2
     return totalError / float(len(points))
 
@@ -26,22 +26,33 @@ def step_gradient(b_current, m_current, points, learningRate):
     m_gradient = 0
     N = float(len(points))
     for i in range(0, len(points)):
-        x = i
-        y = points[i]
+        x = points[i][0]
+        y = points[i][1]
         b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
         m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
     new_b = b_current - (learningRate * b_gradient)
     new_m = m_current - (learningRate * m_gradient)
     return [new_b, new_m]
 
-def gradient_descent_runner(points, starting_b, starting_m, learning_rate, step_volume):
+def gradient_descent_runner(graf, points, starting_b, starting_m, learning_rate, step_volume):
+    '''points2 = []
+    for point in points:
+        point = point * 2000 # need to remove decimals because they mess up algo
+        points2.append(point)
+    points = points2
+
+    for point in points:
+        print("point: ",point)'''
     b = starting_b
     m = starting_m
+    addLineToGraf(graf, b, m, len(points))
     for i in range(step_volume):
-        b, m = step_gradient(b, m, points, learning_rate)
+        [b, m] = step_gradient(b, m, points, learning_rate)
+        print("m is now: ",m)
         if (i % SHOW_EVERY_NSTEPS == 0):
-            linepoints = getLinePoints(b, m, points)
-            graf1.add('Prediction', linepoints)
+            #print(">>>>>>inside SHOW_EVERY_NSTEPS loop")
+            addLineToGraf(graf, (b), m, len(points))
+            #print(xys)
             costvals.append(compute_error_for_line_given_points(b, m, points))
     return [b, m]
 
@@ -69,20 +80,29 @@ def normalise_data(vals):
         new_vals.append(val)
     return new_vals
 
-def addLineToGraf(b, m, amount_of_vals, graf):
-    incrementby = 0.000001
+def addLineToGraf(graf, b, m, amount_of_vals):
+    #print(">>>inside addLineToGraf")
+    amount_of_vals = int(amount_of_vals)
+    incrementby = float(1) / amount_of_vals
+    #incrementby = 1 # use this line instead on non-normalised data
+    #print("amount_of_vals: ",amount_of_vals)
+    #print("increment: ",float(1) / amount_of_vals)
     linepoints = []
     last_y = b
     linepoints.append((0, last_y))
     for i in range(amount_of_vals - 1):
-        last_y = last_y + (m * 1) * incrementby
+        last_y = last_y + (m * incrementby)
+        #print("point: (",(i + 1)*incrementby,",",last_y,")")
         linepoints.append(((i + 1)*incrementby, last_y))
+    graf.add('Prediction', linepoints)
+    #graf.add('TEST',[(0.4,0.4),(0.5,0.3),(0.6,0.2),(0.7,0.7),(0.8,0.8),(0.8,0.9)])
     return linepoints
 
 ####################################################################################################
 #################### Run ####################
 def run():
     xy_vals = [[],[]]
+    norm_xy_vals = [[],[]]
     with open('week1.csv', 'r') as file:
         reader = csv.reader(file)
         next(file)
@@ -92,50 +112,39 @@ def run():
             xy_vals[1].append(float(row[1]))
             i = i + 1
     
-    xy_vals[1] = normalise_data(xy_vals[1])
-    '''for val in xy_vals[0]:
-        print(val)'''
-    xy_vals[0] = normalise_data(xy_vals[0])
-    '''for val in xy_vals[0]:
-        print(val)'''
-    points = xy_vals[1]
-    learning_rate = 0.000000002
-    initial_b = 0.5 # initial y-intercept guess
-    initial_m = -0.4 # initial slope guess
-    amount_of_steps = 5000
+    norm_xy_vals[1] = normalise_data(xy_vals[1])
+    norm_xy_vals[0] = normalise_data(xy_vals[0])
+    xy_vals[1] = norm_xy_vals[1]
+    xy_vals[0] = norm_xy_vals[0]
+    points = zip(xy_vals[0], xy_vals[1])
+
+    learning_rate = 0.00001
+    initial_b = 0.85 # initial y-intercept guess
+    initial_m = 0 # initial slope guess
+    amount_of_steps = 2000
     print ("\n\nStarting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points)))
     print ("Running...")
-    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, amount_of_steps)
+    [b, m] = gradient_descent_runner(graf1, points, initial_b, initial_m, learning_rate, amount_of_steps)
     print ("After {0} iterations b = {1}, m = {2}, error = {3}".format(amount_of_steps, b, m, compute_error_for_line_given_points(b, m, points)))
-    
-    xys = zip(xy_vals[0], xy_vals[1])
-    graf1 = pygal.XY(stroke=False)
     graf1.title = 'pls werk'
-    graf1.add('Values', xys)
-    graf1.add('Prediction', linepoints)
-    graf1.render_to_file('graf.svg')
-
+    graf1.add('Values', points)
+    graf1.render_to_file('graf1.svg')
+'''
     initial_b = 0.5 # initial y-intercept guess
     initial_m = 0 # initial slope guess
     amount_of_steps = 0
     print ("\n\nStarting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points)))
     print ("Running...")
-    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, amount_of_steps)
+    [b, m] = gradient_descent_runner(graf2, points, initial_b, initial_m, learning_rate, amount_of_steps)
     print ("After {0} iterations b = {1}, m = {2}, error = {3}".format(amount_of_steps, b, m, compute_error_for_line_given_points(b, m, points)))
-
-    linepoints = getLinePoints(b, m, xy_vals[1])
-    '''for val in linepoints:
-        print("val:",val)'''
-
     graf2.title = 'gradient_descent_grafaroo'
-    graf2.add('Values', xys)
+    graf2.add('Values', points)
     graf2.render_to_file('graf2.svg')
 
     costvstime_graf.title = 'Cost function values for learning rate = 0.000000002'
     costvstime_graf.add('Cost values', costvals)
     costvstime_graf.x_labels = map(str, range(0, 1))
-    
     costvstime_graf.render_to_file('costvstime_graf.svg')
-
+'''
 if __name__ == '__main__':
     run()
